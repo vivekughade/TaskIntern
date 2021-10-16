c4474f3df1507485cb0451e564646a9f8e456fef
vivekughade <62685829+vivekughade@users.noreply.github.com>
2021-10-16 10:07:53 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -12,19 +12,33 @@ const Signup = () => {
         <div className="container">
         <Bar/>
             <form>
-                <label for="name">Full Name</label></br>
-                <input type="text" value=""/></br>
-                <label for="email">Email</label></br>
-                <input type="email" value=""/></br>
-                <label for="number">Mobile Number</label></br>
-                <input type="number" value=""/></br>
-                <label for="Gender">Gender</label></br>
-                <input type="text" value=""/></br>
-                <label for="password">Password</label></br>
-                <input type="password" value=""/></br>
-                <label for="password">Password</label></br>
-                <input type="password" value=""/></br>
-                <button>Register</button>
+                <div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label ">Name</label>
+  <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label ">Email</label>
+  <input type="email" className="form-control" id="exampleFormControlTextarea1" rows="3"/>
+</div>
+         <div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label c">Name</label>
+  <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name"/>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label c">Mobile Number</label>
+  <input type="number" className="form-control" id="exampleFormControlTextarea1" rows="3"/>
+</div>
+            <div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label c">Password</label>
+  <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="password"/>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label c">Confirm password</label>
+  <input type="password"  className="form-control" id="exampleFormControlTextarea1" rows="3"/>
+</div>
+    <div class="mb-3">
+    <button class="btn btn-dark">Submit</button>
+    </div>
                 <div className="f2">
                     <p></p>Not a member Yet<a href="./Login.jsx">Login</a>
                 </div>
